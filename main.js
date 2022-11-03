066666ba182a2bf469759eb811c180756d88c67b
Andrew <andrew@eseeds.com>
2022-11-03 16:14:21 +0000

    Kata complete

--- main.js
+++ main.js
@@ -1,5 +1,35 @@
+/*PLAN
+Create 3 arrays in separate file, with 1 figure in common
+Export from arrays.js, import into main.js
+Write instructions for kata
+Populate arrays
+Write tests for function
+Write working function to complete kata
+Leave starting function for users
+}
+*/
+
 /* 
 👉 Write your kata here!
+You are approaching a door that requires a passcode. This pin can be found from two different arrays, taking one digit from each. 
+The clue provided to you states that the two arrays have one digit in common. The digits needed to calculate the passcode are one index prior to the common digit.
+Return the sum of the two digits.
+
+For example
+If you have arrays of [ 1, 2, 3, 4, 5 ] and [ 9, 8, 7, 6, 5], the pin numbers will be 4 and 6, both one index before the common number 5. The return would be 10.
 */
 
 //👉 Write the function your CodeWarriors will start with below here:
+export default function inCommon(a, b) {
+    for(let i = 0; i < a.length; i++){
+        if(b.includes(a[i])){
+          for(let j = 0; j < b.length; j++){
+            if(b[j] === a[i]){
+                let second = b[j-1]
+                let first = a[i-1]
+                return (first + second)
+            }
+          }
+        }
+    }
+};
--- main.test.js
+++ main.test.js
@@ -1,1 +1,25 @@
 //👉 Write your tests below here:
+import { expect, test } from '@jest/globals'
+import inCommon from './main.js'
+
+
+test('Test 1', () => {
+    const a = [1, 1, 1, 1, 1, 1, 1, 1, 1, 6, 8, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1];
+    const b = [2, 2, 2, 2, 2, 4, 8, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2];
+    expect(inCommon(a ,b)).toStrictEqual(10);
+});
+test('Test 2', () => {
+    const a = [1, 1, 1, 1, 1, 2, 3, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1];
+    const b = [2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 3, 2, 2, 2, 2, 2, 2, 2, 2];
+    expect(inCommon(a ,b)).toStrictEqual(4);
+});
+test('Test 3', () => {
+    const a = [1, 1, 1, 1, 1, 3, 6, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1];
+    const b = [2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 4, 6, 2, 2, 2, 2, 2, 2];
+    expect(inCommon(a ,b)).toStrictEqual(7);
+});
+test('Test 4', () => {
+    const a = [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 16523, 54, 1, 1, 1, 1, 1, 1, 1];
+    const b = [2, 2, 2, 2, 2, 2, 2, 2, 200, 54, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2];
+    expect(inCommon(a ,b)).toStrictEqual(16723);
+});
